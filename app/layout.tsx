1052eff04b8cf86fbc4d2f8aab9292efee1f97dc
Jonas Stream <jonasstream.oficial@gmail.com>
2026-04-20 18:03:25 -0500

    cambios

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Jonas Stream | Oficial",
+  title: "Jonas Stream | Plataforma Oficial",
   description:
     "Tu proveedor de confianza en plataformas de streaming, música y accesos digitales premium.",
 };
